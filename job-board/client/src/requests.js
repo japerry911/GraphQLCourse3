3d6e7bd750b75aabe7e13a4b234c8f4600987d8e
japerry911 <jack.e.perry@outlook.com>
2020-09-09 09:35:05 -0600

    queries lessons completed

---
+++ job-board/client/src/requests.js
@@ -0,0 +1,70 @@
+const ENDPOINT_URL = "http://localhost:9000/graphql";
+
+async function graphqlRequest(query, variables = {}) {
+  const response = await fetch(ENDPOINT_URL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ query, variables }),
+  });
+
+  const responseBody = await response.json();
+
+  if (responseBody.errors) {
+    const message = responseBody.errors
+      .map((error) => error.message)
+      .join("\n");
+    throw new Error(`GraphQL Error: ${message}`);
+  }
+
+  return responseBody.data;
+}
+
+export async function loadJobs() {
+  const query = `query JobsQuery {
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const { jobs } = await graphqlRequest(query);
+
+  return jobs;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }`;
+  const { job } = await graphqlRequest(query, { id });
+
+  return job;
+}
+
+export async function loadCompany(id) {
+  const query = `query CompanyQuery($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        id
+        title
+      }
+    }
+  }`;
+  const { company } = await graphqlRequest(query, { id });
+
+  return company;
+}
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,5 +1,18 @@
+const db = require("./db");
+
 const Query = {
-  greeting: () => "Hello World",
+  jobs: () => db.jobs.list(),
+  job: (parentValue, args) => db.jobs.get(args.id),
+  company: (parentValue, args) => db.companies.get(args.id),
 };
 
-module.exports = { Query };
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
+module.exports = { Query, Company, Job };
